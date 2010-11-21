4ac9890617bb5ea4695c0bb9b13848f869126098
Patrick Veverka <patrick@veverka.net>
2010-11-21 18:15:04 -0500

    files

--- DMDesktopWeb/callback.php
+++ DMDesktopWeb/callback.php
@@ -9,16 +9,11 @@
 </head>
 <body>
 <?php
+require_once './adodb_lite/adodb.inc.php';
+$db = ADONewConnection('mysql');
+$result = $db->PConnect("mysql.veverka.net", "importerama", "fhsp38ng!", "importerama");
 echo $_GET["code"];
-$link = mysql_connect('mysql.veverka.net', 'importerama', 'fhsp38ng!');
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-mysql_close($link);
-?>
-<?php
-
+$db->Execute("INSERT INTO  `importerama`.`access_tokens` (`id`, `hash`, `access_token`) VALUES ('',  '" . mysql_real_escape_string($_GET["hash"]) . "', '" . mysql_real_escape_string($_GET["code"]) . "')");
 ?>
 </body>
 </html>
