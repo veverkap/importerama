217d3fad3792adac05e6c3e4d2c0868f7111036c
Patrick Veverka <patrick@veverka.net>
2010-11-21 09:32:40 -0500

    files

--- DMDesktopWeb/callback.php
+++ DMDesktopWeb/callback.php
@@ -1,4 +1,24 @@
+<html>
+<head>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		self.close();
+	});
+</script>
+</head>
+<body>
 <?php
-header("Content-Type: text/plain");
 echo $_GET["code"];
+$link = mysql_connect('mysql.veverka.net', 'importerama', 'fhsp38ng!');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
 ?>
+<?php
+
+?>
+</body>
+</html>
