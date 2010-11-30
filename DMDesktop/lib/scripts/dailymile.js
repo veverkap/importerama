1c70c0478daa102fbe4e1366e174cb06b39aa6d9
Patrick Veverka <patrick@veverka.net>
2010-11-29 19:05:00 -0500

    files

--- DMDesktop/lib/scripts/dailymile.js
+++ DMDesktop/lib/scripts/dailymile.js
@@ -49,18 +49,51 @@ function textDateString(dateObj) {
 	return output;
 }
 
+function workoutDetails(d) {
+	if (d.workout != null) {
+		var output = " - ";
+		if (d.workout.distance != null) {
+			output += " " + d.workout.distance.value + " " + d.workout.distance.units;
+		}
+		if (d.workout.duration != null) {
+			output += " / " + Math.floor(d.workout.duration/60) + " minutes";
+		}
+		if (d.workout.felt != null) {
+			output += " / felt " + d.workout.felt;
+		}
+		return output;
+	} else {
+		return "";
+	}
+}
+
 function YouAndFriends() {
 	$.getJSON('http://importerama.veverka.net/friends.json', function(data) {
 		$('#demo-contact').items(data.entries).chain(
 				/* Custom data binding */
 				{
+					'.commentsdiv': {
+						style: 'display: none;',
+						id: function(d,el) {
+							return 'comments_' + d.id;
+						}
+					},
+					'.view_comments': {
+						style: function(d,el) {
+							if (d.comments.length == 0) {
+								return "display: none;";
+							}
+						},
+						content: function(d,el) {
+							return " View Comments (" + d.comments.length + ") - ";
+						},
+						onclick: function(d,el) {
+							return "$('#comments_" + d.id + "').show();";
+						}
+					},
 					'.workout_specs': {
 						content: function(d,el) {
-							if (d.workout != null) {
-								return " - ";
-							} else {
-								return "";
-							}
+							return workoutDetails(d);
 						}
 					},
 					'.workout_type': {
@@ -98,33 +131,8 @@ function YouAndFriends() {
 					},
 					'.datetime': {
 						content: function(d,el) {
-							var today = new Date();
-							var x = d.created_at.toString();
-							var yearx = x.substring(0,4);
-							var monthx = parseInt(x.substring(5,7));
-							monthx = monthx-1;
-							var dayx = x.substring(8,10);
-							var hourx = parseInt(x.substring(11,13));
-							var minutex = x.substring(14,16);
-							var secondx = x.substring(17,19);
-							var gmtHours = -today.getTimezoneOffset()/60;
-							hourx = hourx + gmtHours;
-							var createdat = new Date(yearx, monthx, dayx, hourx, minutex, secondx, 0);
-							var seconds = Math.ceil((createdat.getTime()-today.getTime()) / 1000) * -1;
-							var minutes = Math.ceil(seconds / 60);
-							var hours = Math.ceil(minutes / 60);
-							var days = Math.ceil(hours / 24);
-							var output = "";
-							if (days > 1) {
-								output = days + " days ago";
-							}
-							if (hours <= 24) {
-								output = hours + " hours ago";
-							}
-							if (minutes <= 60) {
-								output = minutes + " minutes ago";
-							}
-							return output;
+							var dateObj = loadDate(d.created_at.toString());
+							return textDateString(dateObj);
 						}
 					}
 
