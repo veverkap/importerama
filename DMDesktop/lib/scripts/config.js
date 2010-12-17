d9ea89fab27c967aac5f3af225b19ae2f23477f8
unknown <A138729@.GEICO.corp.net>
2010-12-17 18:23:09 -0500

    ifle

--- DMDesktop/lib/scripts/config.js
+++ DMDesktop/lib/scripts/config.js
@@ -1,5 +1,10 @@
 var dmDesktopURL = "http://dmdesktop.veverka.net/";
-var dailyMileURL = "http://dmdesktop.veverka.net/";
-//var dailyMileURL = "https://api.dailymile.com/";
 var clientID = "dk91QOkPgzChAP8oqBOB5gUKEk8afmDF7demeCtS";
-var oauthURL = dailyMileURL + "oauth/authorize?response_type=code&client_id=" + clientID + "&redirect_uri=" + dmDesktopURL + "callback.php";
+
+//PRODUCTION URLS
+//var dailyMileURL = "https://api.dailymile.com/";
+//var oauthURL = dailyMileURL + "oauth/authorize?response_type=code&client_id=" + clientID + "&redirect_uri=" + dmDesktopURL + "callback.php";
+
+//TESTING URLS
+var dailyMileURL = "http://dmdesktop.veverka.net/";
+var oauthURL = dailyMileURL + "oauth/authorize.php?response_type=code&client_id=" + clientID + "&redirect_uri=" + dmDesktopURL + "fauxcallback.php";
